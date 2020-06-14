12db3187763ef93e27829996d00442990a836a23
Daniel <visual.eyes.success@gmail.com>
2020-06-14 17:47:45 +0200

    noticing latency issues in console of RStudio
    2 minute console latency while initially displaying results of ro_stations (stations count in ea river)

--- 00_init_eda_final.R
+++ 00_init_eda_final.R
@@ -6,29 +6,30 @@ runoff_info[, ]
 #1
 #Where are the Stations located-
 #Countries
-#show all stations and the country
-for(country in 1:length(runoff_info[, Country])){
+#messy version shows all stations and the country
+for(country in 1:length(runoff_info[, Country]) ){
   str(runoff_info[country, Country, by=Station])
 }
-#clean version
+#clean version showing all countries
 ro_country_count <- data.table(Country = unique(runoff_info[, Country]))
 ro_country_count
 #19 different countries
 
 #Continents
+station_continents <- runoff_info[, .(Station, Continent) ]
+station_continents
 for(notEU in 1:length(runoff_info[, Continent]) ){
   if (runoff_info[notEU, Continent] !='EU'){
     str(runoff_info[notEU, Continent, by=Station] ) 
     #returning  nothing since all countries are categorized as 'EU'   
   } 
 } 
-#All Stations are in 'EU', Russia described as EU...Russia is not European.
+#All Stations are in 'EU', Russia described as EU...Russia is not Europe technically.
+
 #2 
-#How many stations/rivers exist per country?
-station_continents <- runoff_info[, .(Station, Continent) ]
-station_continents
+#How many *stations exist per country?
 ro_countries <- data.table(Country = unique(runoff_info[, Country]), Stations=0)
-#use for loop to count occurances of rivers in ea country
+#use for loop to count occurances of stations in ea country
 for(a in 1:length(runoff_info[, Station]) ){
   for(b in 1:length(ro_countries[, Country]) ){
     if(runoff_info[a, Country] == ro_countries[b, Country] ){
@@ -36,8 +37,34 @@ for(a in 1:length(runoff_info[, Station]) ){
     }
   }
 }
-ro_countries
-#here we see the count of the rivers in ea country, minimum count 1, max 78(Germany)
+ro_countries #here we see the count max 78 in Germany
+#2 cont'd--How many rivers* exist per country?
+ro_rivers <- data.table(Country = unique(runoff_info[, Country]), Rivers=0)
+ro_rivers
+for(a in 1:length(runoff_info[, River]) ){
+  for(b in 1:length(ro_rivers[, Country]) ){
+    if(runoff_info[a, Country] == ro_rivers[b, Country] ){
+      ro_rivers[b, 2] <- ro_rivers[b, 2]+1
+    }
+  }
+}
+ro_rivers #here we see the count max again, 78 in Germany
+#when doing the loops for stations/rivers count per country the results were the same
+
+#3 how many stations exist per river? 
+ro_stations <- data.table(River = unique(runoff_info[, River]), Stations=0)
+ro_stations
+for(a in 1:length(runoff_info[, Station]) ){
+  for(b in 1:length(ro_stations[, River]) ){
+    if(runoff_info[a, River] == ro_stations[b, River] ){
+      ro_stations[b, 2] <- ro_stations[b, 2]+1
+    }
+  }
+}
+ro_stations ##RStudio major latency noted in console
+most_stations_river <- max(ro_stations$Stations)
+most_stations_river #danube has 13 stations, max count
+
 
 
 
