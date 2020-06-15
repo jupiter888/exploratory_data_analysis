a97362f5ea51a5acaa5e1fd5ecc77594fcc250e6
Daniel <visual.eyes.success@gmail.com>
2020-06-15 14:03:20 +0200

    coefficient variation

--- 01_exploration_eda_final.R
+++ 01_exploration_eda_final.R
@@ -8,13 +8,8 @@ runoff_stats <- runoff_day[, .(mean_day = round(mean(value), 0),
                                min_day = round(min(value), 0),
                                max_day = round(max(value), 0)), by = id]
 runoff_stats
-#NEED TO CONNECT ID WITH STATION NAME
-#
-#coefficient of variance=stdv/mean
 #looking into runoff_year
 runoff_year
-
-
 
 runoff_median <- runoff_day[, .(round(median(value),0)), by = id]
 runoff_stats$median <- runoff_median$V1
@@ -27,4 +22,10 @@ ggplot(runoff_stats) +
   geom_point(aes(x= id, y= min_day, col=colset_4[3]), shape = shapeset_4[3]) +
   geom_point(aes(x= id, y= max_day, col=colset_4[4]), shape = shapeset_4[4]) +
   labs(y="Runoff", x = "Station id", color = 'Runoff value type')
-#how to get that many station id's 
+
+##
+#coefficient of variance=stdv/mean
+runoff_coefficient_variation <- runoff_day[, .(coeff_var_day = sd(value)/mean(value)), by = id]
+runoff_coefficient_variation 
+
+
